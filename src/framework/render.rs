f346454af319a79502ebee4e0fb43bba0f363e0f
minikin <djminikin@gmail.com>
2025-01-06 22:25:23 +0100

    chore: add rust lints

--- src/framework/render.rs
+++ src/framework/render.rs
@@ -1,6 +1,7 @@
 use wgpu;
 use winit::window::Window;
 
+/// Render is a wrapper around wgpu's surface, device, queue, and configuration.
 struct Render {
     surface: wgpu::Surface,
     device: wgpu::Device,
@@ -9,14 +10,17 @@ struct Render {
 }
 
 impl Render {
+    /// Create a new Render instance.
     pub fn new(window: &Window) -> Self {
         todo!()
     }
 
+    /// Render a frame.
     pub fn render(&self) {
         todo!()
     }
 
+    /// Resize the render target.
     pub fn resize(&mut self, width: u32, height: u32) -> Result<(), wgpu::SurfaceError> {
         todo!()
     }
